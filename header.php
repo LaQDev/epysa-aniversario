76ee5fcace5dee905de981d8759a892bb09ff940
LaQ Developer <github@laq.cl>
2026-04-28 17:27:00 -0400

    Update header.php

--- header.php
+++ header.php
@@ -2,6 +2,11 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TZH8GXBB');</script>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
@@ -10,6 +15,8 @@
 </head>
 
 <body <?php body_class(); ?>>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZH8GXBB"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <?php wp_body_open(); ?>
 
     <?php
@@ -103,5 +110,3 @@
             </div>
         </div>
     </header>
-
-    
